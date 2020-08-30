442e0545822c6226fef88484cba89a39849ae8dc
varoOP <vardamrohit123@gmail.com>
2020-08-30 16:58:35 +0530

    Formatted code

--- src/index.js
+++ src/index.js
@@ -30,13 +30,13 @@ app.intent('search_anime', async (conv, params) => {
   if (data.media.length >= 1) {
     if (data.media.length === 1)
       conv.data.exactMatch = true;
-      if(!conv.data.exactMatch)
-     for(i=0; i<data.media.length; i++){
-         if(deal.dealwithtitle(data.media[i]).toLowerCase() == searchfor.toLowerCase()){
-         conv.data.exactMatch = true;
-         console.log(`Exact matched: `+deal.dealwithtitle(data.media[i]).toLowerCase());
-         }
-     }
+    if (!conv.data.exactMatch)
+      for (i = 0; i < data.media.length; i++) {
+        if (deal.dealwithtitle(data.media[i]).toLowerCase() == searchfor.toLowerCase()) {
+          conv.data.exactMatch = true;
+          console.log(`Exact matched: ` + deal.dealwithtitle(data.media[i]).toLowerCase());
+        }
+      }
     if (!conv.data.exactMatch) {
       for (let i = 0; i < data.media.length; i++) {
         id_array.push(data.media[i].id);
@@ -44,80 +44,80 @@ app.intent('search_anime', async (conv, params) => {
       for (let i = 0; i < id_array.length; i++) {
         const query = await Anilist.media.anime(id_array[i]);
         query.sortthis = i + 1;
-        if(!query.isAdult)
-        search_result.push(query);
+        if (!query.isAdult)
+          search_result.push(query);
       }
-      if(search_result.length > 1){
-      let carouselItems = {};
-      let idtokeymap = {};
-      search_result.forEach(animu => {
-        let itemdetails = {
-          title: `${deal.dealwithtitle(animu)}`,
-          image: new Image({
-            url: animu.coverImage.large,
-            alt: `Cover image of ${deal.dealwithtitle(animu)}.`,
-          }),
-        };
-        let map = {
-          key: animu.sortthis,
-          ani_id: animu.id,
-        };
-        carouselItems[animu.sortthis] = itemdetails;
-        idtokeymap[animu.sortthis] = map;
-      });
-      conv.data.store = idtokeymap;
-      console.log(idtokeymap);
-      conv.data.num = carouselItems;
-      conv.ask(`Maybe, your anime is one of these? Please choose one by tapping on it or by calling out it's name.`, new Carousel({
-        title: `Anime Search Result`,
-        items: carouselItems,
-      }));
-      conv.data.var = false;
+      if (search_result.length > 1) {
+        let carouselItems = {};
+        let idtokeymap = {};
+        search_result.forEach(animu => {
+          let itemdetails = {
+            title: `${deal.dealwithtitle(animu)}`,
+            image: new Image({
+              url: animu.coverImage.large,
+              alt: `Cover image of ${deal.dealwithtitle(animu)}.`,
+            }),
+          };
+          let map = {
+            key: animu.sortthis,
+            ani_id: animu.id,
+          };
+          carouselItems[animu.sortthis] = itemdetails;
+          idtokeymap[animu.sortthis] = map;
+        });
+        conv.data.store = idtokeymap;
+        console.log(idtokeymap);
+        conv.data.num = carouselItems;
+        conv.ask(`Maybe, your anime is one of these? Please choose one by tapping on it or by calling out it's name.`, new Carousel({
+          title: `Anime Search Result`,
+          items: carouselItems,
+        }));
+        conv.data.var = false;
       }
-      else if(search_result.length === 1) {
-          const query = search_result[0];
-          const title = deal.dealwithtitle(query);
+      else if (search_result.length === 1) {
+        const query = search_result[0];
+        const title = deal.dealwithtitle(query);
         conv.close(queries.dealwithquery(query), new BasicCard({
-        text: deal.dealwithdescription(query),
-        subtitle: deal.dealwithgenres(query),
-        title: title,
-        buttons: new Button(deal.dealwithbutton(query)),
-        image: new Image({
-          url: deal.dealwithimage(query),
-          alt: `Cover image of ${title}.`,
-        }),
-      }));
+          text: deal.dealwithdescription(query),
+          subtitle: deal.dealwithgenres(query),
+          title: title,
+          buttons: new Button(deal.dealwithbutton(query)),
+          image: new Image({
+            url: deal.dealwithimage(query),
+            alt: `Cover image of ${title}.`,
+          }),
+        }));
       }
       else {
-            conv.ask(`No result found! Please speak clearly or try an alternative name. Would you like to search for it again?`, new Suggestions('Yes','No'));
-            conv.data.var = true;
+        conv.ask(`No result found! Please speak clearly or try an alternative name. Would you like to search for it again?`, new Suggestions('Yes', 'No'));
+        conv.data.var = true;
       }
     }
     else if (conv.data.exactMatch) {
       const query = await Anilist.media.anime(data.media[0].id);
       const title = deal.dealwithtitle(query);
-      if(!query.isAdult)
-      conv.close(queries.dealwithquery(query), new BasicCard({
-        text: deal.dealwithdescription(query),
-        subtitle: deal.dealwithgenres(query),
-        title: title,
-        buttons: new Button(deal.dealwithbutton(query)),
-        image: new Image({
-          url: deal.dealwithimage(query),
-          alt: `Cover image of ${title}.`,
-        }),
-      }));
-      if(query.isAdult){
-      conv.ask(`No result found! Please speak clearly or try an alternative name. Would you like to search for it again?`, new Suggestions('Yes','No'));
-       conv.data.var = true;
+      if (!query.isAdult)
+        conv.close(queries.dealwithquery(query), new BasicCard({
+          text: deal.dealwithdescription(query),
+          subtitle: deal.dealwithgenres(query),
+          title: title,
+          buttons: new Button(deal.dealwithbutton(query)),
+          image: new Image({
+            url: deal.dealwithimage(query),
+            alt: `Cover image of ${title}.`,
+          }),
+        }));
+      if (query.isAdult) {
+        conv.ask(`No result found! Please speak clearly or try an alternative name. Would you like to search for it again?`, new Suggestions('Yes', 'No'));
+        conv.data.var = true;
       }
     }
   }
   else {
-      conv.ask(`No result found! Please speak clearly or try an alternative name. Would you like to search for it again?`, new Suggestions('Yes','No'));
-      conv.data.var = true;
-  } 
-  });
+    conv.ask(`No result found! Please speak clearly or try an alternative name. Would you like to search for it again?`, new Suggestions('Yes', 'No'));
+    conv.data.var = true;
+  }
+});
 
 app.intent('handle_carousel', async (conv, params, option) => {
   const key = parseInt(option);
@@ -138,22 +138,22 @@ app.intent('handle_carousel', async (conv, params, option) => {
 });
 
 app.intent('search_anime - yes', async (conv) => {
-   conv.followup('WELCOME', {}); 
+  conv.followup('WELCOME', {});
 });
 
 app.intent('fallback', async (conv) => {
-    if(!conv.data.var){
-   conv.ask(`Please select one option by tapping on it or calling out it's name.`);
-   conv.ask(`Maybe, your anime is one of these?`, new Carousel({
-        title: `Anime Search Result`,
-        items: conv.data.num,
-      }));
-    }
-    else conv.ask(`Sorry I didn't get that. Would you like to search for that anime again?`, new Suggestions('Yes','No'));
+  if (!conv.data.var) {
+    conv.ask(`Please select one option by tapping on it or calling out it's name.`);
+    conv.ask(`Maybe, your anime is one of these?`, new Carousel({
+      title: `Anime Search Result`,
+      items: conv.data.num,
+    }));
+  }
+  else conv.ask(`Sorry I didn't get that. Would you like to search for that anime again?`, new Suggestions('Yes', 'No'));
 });
 
 app.intent('fallback - yes', async (conv) => {
-   conv.followup('WELCOME', {}); 
+  conv.followup('WELCOME', {});
 });
 
 const expressApp = express().use(bodyParser.json());
--- src/query.js
+++ src/query.js
@@ -1,89 +1,89 @@
 const deal = require(`./functions`);
 module.exports = {
- dealwithquery: function(query) {
-    var title = deal.dealwithtitle(query);
-    var tags = deal.dealwithtags(query);
-    var last = deal.dealwithlast(query);
-    var next = deal.dealwithnextair(query);
-    var startDate = new Date(query.startDate.year, query.startDate.month-1, query.startDate.day);
-    var length = deal.dealwithlength(query);
-    var score = deal.dealwithscore(query);
-    var studio = deal.dealwithstudio(query);
-    var date = deal.dealwithdate(query, title);
-    switch(query.format){
-        case 'MOVIE':
-            if(query.status === 'FINISHED'){
-                return `${title} released on ${startDate.toLocaleDateString('en-US')}${studio}. ${length} ${score} ${tags} ${last}`;
-            }
-            else if(query.status === 'NOT_YET_RELEASED'){
-                return `${date}${studio}. ${next} ${tags} ${last}`;
-            }
+    dealwithquery: function (query) {
+        var title = deal.dealwithtitle(query);
+        var tags = deal.dealwithtags(query);
+        var last = deal.dealwithlast(query);
+        var next = deal.dealwithnextair(query);
+        var startDate = new Date(query.startDate.year, query.startDate.month - 1, query.startDate.day);
+        var length = deal.dealwithlength(query);
+        var score = deal.dealwithscore(query);
+        var studio = deal.dealwithstudio(query);
+        var date = deal.dealwithdate(query, title);
+        switch (query.format) {
+            case 'MOVIE':
+                if (query.status === 'FINISHED') {
+                    return `${title} released on ${startDate.toLocaleDateString('en-US')}${studio}. ${length} ${score} ${tags} ${last}`;
+                }
+                else if (query.status === 'NOT_YET_RELEASED') {
+                    return `${date}${studio}. ${next} ${tags} ${last}`;
+                }
 
-            else if(query.status === `CANCELLED`){
-                return `${title} is a movie that has been cancelled. ${last} Thank you for using Anime Search!`;
-            }
-            else{ 
-                return `${title} is a movie that started releasing on ${startDate.toLocaleDateString('en-US')}${studio}. ${length} ${score} ${tags} ${last} Thank you for using Anime Search!`;
-            }
-            break;
+                else if (query.status === `CANCELLED`) {
+                    return `${title} is a movie that has been cancelled. ${last} Thank you for using Anime Search!`;
+                }
+                else {
+                    return `${title} is a movie that started releasing on ${startDate.toLocaleDateString('en-US')}${studio}. ${length} ${score} ${tags} ${last} Thank you for using Anime Search!`;
+                }
+                break;
 
-        case 'TV':
-        case 'ONA':
-        case 'OVA':
-            if(query.status === 'RELEASING'){
-                return `${title} started airing on ${startDate.toLocaleDateString('en-US')}${studio}. ${next} ${score} ${tags} ${last}`;
-            }
-            
-            else if(query.status === 'FINISHED'){
-                var endDate = new Date(query.endDate.year, query.endDate.month-1, query.endDate.day);
-                return `${title} started airing on ${startDate.toLocaleDateString('en-US')}, finished airing on ${endDate.toLocaleDateString('en-US')} with a total of ${query.episodes} episode(s)${studio}. ${score} ${tags} ${last}`;
-            }
+            case 'TV':
+            case 'ONA':
+            case 'OVA':
+                if (query.status === 'RELEASING') {
+                    return `${title} started airing on ${startDate.toLocaleDateString('en-US')}${studio}. ${next} ${score} ${tags} ${last}`;
+                }
 
-            else if(query.status === 'NOT_YET_RELEASED'){
-                return `${date}${studio}. ${next} ${tags} ${last}`;
-            }
+                else if (query.status === 'FINISHED') {
+                    var endDate = new Date(query.endDate.year, query.endDate.month - 1, query.endDate.day);
+                    return `${title} started airing on ${startDate.toLocaleDateString('en-US')}, finished airing on ${endDate.toLocaleDateString('en-US')} with a total of ${query.episodes} episode(s)${studio}. ${score} ${tags} ${last}`;
+                }
 
-            else if(query.status === 'CANCELLED'){
-                return `${title} is an Anime that has been cancelled. ${last}\n Thank you for using Anime Search!`;
-            }
-            break;
-        
-        case 'TV_SHORT':
-        case 'SPECIAL':
-            if(query.status === 'RELEASING'){
-                return `${title} started airing on ${startDate.toLocaleDateString('en-US')}${studio}. ${length} ${next} ${score} ${tags} ${last}`;
-            }
-            
-            else if(query.status === 'FINISHED'){
-                var endDate = new Date(query.endDate.year, query.endDate.month-1, query.endDate.day);
-                return `${title} started airing on ${startDate.toLocaleDateString('en-US')}, finished airing on ${endDate.toLocaleDateString('en-US')} with a total of ${query.episodes} episode(s)${studio}. ${length} ${score} ${tags} ${last}`;
-            }
-            
-            else if(query.status === 'NOT_YET_RELEASED'){
-                return `${date}${studio}. ${next} ${tags} ${last}`;
-            }
-            
-            else if(query.status === 'CANCELLED'){
-                return `${title} is an Anime that has been cancelled. ${last}\n Thank you for using Anime Search!`;  
-            }
-            break;
-            
-        case 'MUSIC':
-            if(query.status === 'FINISHED'){
-                return `${title} is a music video that released on ${startDate.toLocaleDateString('en-US')}${studio}. ${length} ${score} ${tags} ${last}`;
-            }
-            
-            else if(query.status === 'RELEASING'){
-                return `${title} is a series of music videos that started releasing on ${startDate.toLocaleDateString('en-US')}${studio}. ${length} ${score} ${tags} ${last}`;
-            }
-            
-            else if(query.status === 'CANCELLED'){
-                return `${title} is a music video that has been cancelled. ${last} Thank you for using Anime Search!`;
-            }
-            else return `${title} is a music video. ${tags} ${score} ${last}`;
-            break;
-        
-        default: return `${title} is of the ${query.format.toLowerCase()} format. ${tags} ${last} Thank you for using Anime Search!`;
+                else if (query.status === 'NOT_YET_RELEASED') {
+                    return `${date}${studio}. ${next} ${tags} ${last}`;
+                }
+
+                else if (query.status === 'CANCELLED') {
+                    return `${title} is an Anime that has been cancelled. ${last}\n Thank you for using Anime Search!`;
+                }
+                break;
+
+            case 'TV_SHORT':
+            case 'SPECIAL':
+                if (query.status === 'RELEASING') {
+                    return `${title} started airing on ${startDate.toLocaleDateString('en-US')}${studio}. ${length} ${next} ${score} ${tags} ${last}`;
+                }
+
+                else if (query.status === 'FINISHED') {
+                    var endDate = new Date(query.endDate.year, query.endDate.month - 1, query.endDate.day);
+                    return `${title} started airing on ${startDate.toLocaleDateString('en-US')}, finished airing on ${endDate.toLocaleDateString('en-US')} with a total of ${query.episodes} episode(s)${studio}. ${length} ${score} ${tags} ${last}`;
+                }
+
+                else if (query.status === 'NOT_YET_RELEASED') {
+                    return `${date}${studio}. ${next} ${tags} ${last}`;
+                }
+
+                else if (query.status === 'CANCELLED') {
+                    return `${title} is an Anime that has been cancelled. ${last}\n Thank you for using Anime Search!`;
+                }
+                break;
+
+            case 'MUSIC':
+                if (query.status === 'FINISHED') {
+                    return `${title} is a music video that released on ${startDate.toLocaleDateString('en-US')}${studio}. ${length} ${score} ${tags} ${last}`;
+                }
+
+                else if (query.status === 'RELEASING') {
+                    return `${title} is a series of music videos that started releasing on ${startDate.toLocaleDateString('en-US')}${studio}. ${length} ${score} ${tags} ${last}`;
+                }
+
+                else if (query.status === 'CANCELLED') {
+                    return `${title} is a music video that has been cancelled. ${last} Thank you for using Anime Search!`;
+                }
+                else return `${title} is a music video. ${tags} ${score} ${last}`;
+                break;
+
+            default: return `${title} is of the ${query.format.toLowerCase()} format. ${tags} ${last} Thank you for using Anime Search!`;
+        }
     }
-}
 };
